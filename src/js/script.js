e9e5965446e779470a60c2423c1f7448af8117ef
mmirzafahlefi <nyobatyuk0910@gmail.com>
2024-01-15 01:49:45 +0700

    perbaikan bug murotal

--- src/js/script.js
+++ src/js/script.js
@@ -281,25 +281,62 @@ $.ajax({
 
 // data murotal
 
-$("#daftar-murotal").on("click", `#${getNomor}`, function () {
-  var value = $(this).data("value");
-  var nama = $(this).data("nama");
+// $("#daftar-murotal").on("click", `#${getNomor}`, function () {
+//   var value = $(this).data("value");
+//   var nama = $(this).data("nama");
 
-  audioPlayer = new oyoPlayer();
-  $(".player").append(audioPlayer);
-  audioPlayer.addToPlaylist(value, nama);
-  audioPlayer.setSourceIndex(1);
-  audioPlayer.setNotification("Push Play to start");
+//   audioPlayer = new oyoPlayer();
+//   $(".player").append(audioPlayer);
+//   audioPlayer.addToPlaylist(value, nama);
+//   audioPlayer.setSourceIndex(1);
+//   audioPlayer.setNotification("Push Play to start");
 
-  $(".player audio").on("canplay", function () {
-    const deration = this.duration;
-    let x = Math.trunc(deration);
-    console.log(x + "000");
-    setTimeout(() => location.reload(true), x + "000");
-  });
-  $(".player .oyoplayer").addClass(["mb-[4.5rem]", "lg:mb-0"]);
-  $(".player .oyotagbox").addClass(["w-[50%]", "lg:w-full"]);
-});
+//   $(".player audio").on("canplay", function () {
+//     const deration = this.duration;
+//     let x = Math.trunc(deration);
+//     console.log(x + "000");
+//     setTimeout(() => location.reload(true), x + "000");
+//   });
+//   $(".player .oyoplayer").addClass(["mb-[4.5rem]", "lg:mb-0"]);
+//   $(".player .oyotagbox").addClass(["w-[50%]", "lg:w-full"]);
+// });
+
+// $.ajax({
+//   url: "https://equran.id/api/v2/surat",
+//   type: "get",
+//   dataType: "JSON",
+//   success: function (result) {
+//     let murotal = result.data;
+//     let suara = getSuara;
+//     const selectSuara = $("select#suara");
+
+//     if (selectSuara.val() == suara) {
+//       selectSuara.attr("selected", "selected");
+//     }
+
+//     $("select#suara").val(suara);
+
+//     $.each(murotal, function (i, hasil) {
+//       $("#daftar-murotal").append(
+//         `<div id="${hasil.nomor}" class="lg:p-2 w-full cursor-pointer lg:w-1/4" onclick="getDetailSurah(${hasil.nomor})" data-search="${hasil.namaLatin}" data-value="${hasil.audioFull[suara]}" data-nama="${hasil.namaLatin}">
+//       <div
+//         class="overflow-hidden rounded-lg shadow-md mb-5 bg-primary py-3 px-8"
+//       >
+//         <div class="w-full mb-2">
+//           <h1 class="text-light">${hasil.nomor}. ${hasil.namaLatin}</h1>
+//         </div>
+//         <div class="w-full text-right">
+//           <h1 class="text-2xl font-bold text-white">
+//           ${hasil.nama}
+//           </h1>
+//           <p class="text-base font-sm text-light">${hasil.tempatTurun} - ${hasil.arti}</p>
+//         </div>
+//       </div>
+//     </div>`
+//       );
+//     });
+//   },
+// });
 
 $.ajax({
   url: "https://equran.id/api/v2/surat",
@@ -308,7 +345,11 @@ $.ajax({
   success: function (result) {
     let murotal = result.data;
     let suara = getSuara;
+    let currentSongIndex = 0;
+    let isPlaying = false;
+    const playlist = $("#daftar-murotal");
     const selectSuara = $("select#suara");
+    const myAudio = $("#my-audio");
 
     if (selectSuara.val() == suara) {
       selectSuara.attr("selected", "selected");
@@ -317,8 +358,8 @@ $.ajax({
     $("select#suara").val(suara);
 
     $.each(murotal, function (i, hasil) {
-      $("#daftar-murotal").append(
-        `<div id="${hasil.nomor}" class="lg:p-2 w-full cursor-pointer lg:w-1/4" onclick="getDetailSurah(${hasil.nomor})" data-search="${hasil.namaLatin}" data-value="${hasil.audioFull[suara]}" data-nama="${hasil.namaLatin}">
+      playlist.append(
+        `<div id="murotal" class="lg:p-2 w-full cursor-pointer lg:w-1/4" data-id="${hasil.nomor}" data-value="${hasil.audioFull[suara]}" data-nama="${hasil.namaLatin}">
       <div
         class="overflow-hidden rounded-lg shadow-md mb-5 bg-primary py-3 px-8"
       >
@@ -335,6 +376,81 @@ $.ajax({
     </div>`
       );
     });
+
+    playlist.on("click", "#murotal", function () {
+      currentSongIndex = $(this).data("id");
+      $(".player").removeClass("hidden").addClass("block");
+      playSong();
+    });
+
+    $("#playPauseBtn").on("click", function () {
+      const audioPlayer = myAudio[0];
+
+      if (isPlaying) {
+        audioPlayer.pause();
+        $("#playPauseBtn i").removeClass("bx-play").addClass("bx-pause");
+      } else {
+        audioPlayer.play();
+        $("#playPauseBtn i").removeClass("bx-pause").addClass("bx-play");
+      }
+
+      isPlaying = !isPlaying;
+    });
+
+    // Update progress bar during audio playback
+    myAudio.on("timeupdate", function () {
+      const audioPlayer = this;
+      const progressBar = $("#progressBar");
+      const currentTimeDisplay = $("#currentTime");
+      const totalTimeDisplay = $("#totalTime");
+      const titleDisplay = $("#judul");
+
+      const progress = (audioPlayer.currentTime / audioPlayer.duration) * 100;
+      progressBar.css("width", progress + "%");
+
+      // Update current time display
+      const minutes = Math.floor(audioPlayer.currentTime / 60);
+      const seconds = Math.floor(audioPlayer.currentTime % 60);
+      const formattedTime = padZero(minutes) + ":" + padZero(seconds);
+      currentTimeDisplay.text(formattedTime);
+
+      // Update total duration display
+      const totalHours = Math.floor(audioPlayer.duration / 3600);
+      const totalMinutes = Math.floor((audioPlayer.duration % 3600) / 60);
+      const totalSeconds = Math.floor(audioPlayer.duration % 60);
+      const formattedTotalTime =
+        padZero(totalHours) +
+        ":" +
+        padZero(totalMinutes) +
+        ":" +
+        padZero(totalSeconds);
+      totalTimeDisplay.text(formattedTotalTime);
+
+      // Update title display
+      titleDisplay.text(murotal[currentSongIndex - 1].namaLatin);
+    });
+
+    // Handle click on progress bar to seek
+    $(".audio-progress").on("click", function (e) {
+      const audioPlayer = $("#audioPlayer")[0];
+      const progressBar = $("#progressBar");
+      const seekPosition = (e.pageX - $(this).offset().left) / $(this).width();
+
+      audioPlayer.currentTime = audioPlayer.duration * seekPosition;
+      progressBar.css("width", seekPosition * 100 + "%");
+    });
+
+    function padZero(num) {
+      return (num < 10 ? "0" : "") + num;
+    }
+
+    function playSong() {
+      const srcUrl = murotal[currentSongIndex - 1].audioFull[suara];
+
+      myAudio.attr("src", srcUrl);
+      myAudio[0].load();
+      myAudio[0].play();
+    }
   },
 });
 
